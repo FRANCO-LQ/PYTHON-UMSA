16badc051d745cbb0c9c542f800434250cc94cf4
Franco <jackfrac78@gmail.com>
2024-04-26 20:50:18 -0400

    first commit

---
+++ operadores.py
@@ -0,0 +1,35 @@
+#1
+print(17 %3)
+print (3**2)
+print(24//9)
+
+a = 5
+b = 8
+a +=b
+print(a)
+
+a1=6
+a2=8
+a1 -= a2
+print(a1)
+
+b1=5
+b2=2
+b1 *=b2
+print(b1)
+
+c1= 12
+c2= 4
+c1 /= c2
+print(c1)
+
+d1=6
+d2=2
+
+print(5==5)
+print(5 !=5)
+print(5 > 8)
+print(5 < 8)
+print(3 <= 10)
+print(3 >= 10)
+print(10 >= 10)
